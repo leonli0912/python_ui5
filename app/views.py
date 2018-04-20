68924bb7f520073a97b235d526209dc59e1d8264
Leon Li <leon.li02@sap.com>
2018-04-20 10:01:13 +0800

    upload success

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
-from flask import render_template
+from flask import render_template,jsonify,request
 from app import app
 
 @app.route('/')
-@app.route('/index')
 def index():
+    return 'welcome!'
+@app.route('/load')
+def load():
     user = { 'nickname': 'Miguel' } # fake user
     posts = [ # fake array of posts
         {
@@ -19,3 +21,28 @@ def index():
         title = 'Home',
         user = user,
         posts = posts)
+@app.route('/upload/',methods=['POST'])
+def start_parse():
+    if request.method == 'POST': 
+        filename = 'initial'       
+        f = request.files['myFileUpload']
+        filename = f.filename
+        minetype = f.content_type
+        f.save('./'+filename)
+    return filename
+
+@app.route('/result',methods=['GET'])
+def get_xml():
+    xml = [{
+            'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },{
+            'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+    ]
+    return jsonify({'xmls':xml}),200
